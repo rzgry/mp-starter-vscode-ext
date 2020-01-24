4d3860a38b979731180e040069ff21ecfa1d6d0a
Ryan Zegray <ryan.zegray@gmail.com>
2020-01-24 10:32:44 -0500

    more update

--- src/properties.ts
+++ src/properties.ts
@@ -1,13 +1,3 @@
-export const MP_SERVER_LABELS: Record<string, string> = {
-    LIBERTY: "Open Liberty",
-    HELIDON: "Helidon",
-    PAYARA_MICRO: "Payara Micro",
-    THORNTAIL_V2: "Thorntail Version 2",
-    KUMULUZEE: "KumuluzEE",
-    TOMEE: "Apache TomEE 8.00-M2",
-    WILDFLY_SWARM: "WildFly Swarm"
-};
-
 export const MP_VERSION_LABELS: Record<string, string> = {
     MP32: "Version 3.2",
     MP30: "Version 3.0",
@@ -17,4 +7,14 @@ export const MP_VERSION_LABELS: Record<string, string> = {
     MP14: "Version 1.4",
     MP13: "Version 1.3",
     MP12: "Version 1.2"
+};
+
+export const MP_SERVER_LABELS: Record<string, string> = {
+    LIBERTY: "Open Liberty",
+    HELIDON: "Helidon",
+    PAYARA_MICRO: "Payara Micro",
+    THORNTAIL_V2: "Thorntail Version 2",
+    KUMULUZEE: "KumuluzEE",
+    TOMEE: "Apache TomEE 8.00-M2",
+    WILDFLY_SWARM: "WildFly Swarm"
 };
